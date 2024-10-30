d20f6161456a5b31fc0b6a48b5bfda3742dcb9b9
José Enrique Cerezo <joseenrique61@hotmail.com>
2024-10-30 08:20:55 -0500

    Agregar archivos de proyecto.

---
+++ CerezoJose_Progreso1/Models/JoseCerezo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CerezoJose_Progreso1.Models
+{
+	public class JoseCerezo
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string Nombre { get; set; }
+
+		[Required]
+		public int CantidadHermanos { get; set; }
+
+		[Required]
+		public double SaldoBancario { get; set; }
+
+		[Required]
+		public bool EsHombre { get; set; }
+
+		[Required]
+		public DateTime FechaNacimiento { get; set; }
+	}
+}
